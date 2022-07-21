4c5a9819405d4208c935da9b7d8ad7e1c8bfcc2f
kevin.delRio <kevindelrio1993@gmail.com>
2022-07-21 20:50:30 -0300

    validador de texto y numeros

--- javaScript/main.js
+++ javaScript/main.js
@@ -7,26 +7,51 @@ class Producto {
         this.stock = stockP;
     }
 }
+// ----------------------------------
 // productos
 let producto1 = new Producto(1, "pelotas", 250, 150);
 let producto2 = new Producto(2, "lapiceras", 100, 120);
 let producto3 = new Producto(3, "tazas", 70, 170);
 let producto4 = new Producto(4, "marcadores", 150, 230);
-
+// ----------------------------------
 // agrego productos a la tienda
 const tienda = [producto1, producto2, producto3, producto4];
-
+// ----------------------------------
+// validar numero
+function validarNumber (e){
+    while (e == null || /\D/.test(e) || e == "") {
+        e = parseInt(prompt("Entre un número VÁLIDO: "));
+    };
+    return e;
+}
+// ----------------------------------
+//validar texto
+function validarTexto(t) {
+  let num = /[0-9]+/; 
+    while (num.test(t) || t == "" ) {
+        t = prompt("Ingrese un nombre VÁLIDO: ");
+    }
+    return t;
+}
+// ----------------------------------
 // agrego productos a la tienda por prompt
 function agregarProducto() {
-    let id = parseInt(prompt(" Ingrese id: "));
+    let id = parseInt(prompt("Ingrese id: "));
+    id = validarNumber(id);
+
     let nombre = prompt("Nombre del producto: ");
+    nombre = validarTexto(nombre);
+
     let precio = parseInt(prompt("Precio del producto:"));
-    let stock = parseInt(prompt(" Ingrese stock: "));
+    precio = validarNumber(precio);
+    let stock = parseInt(prompt(" Ingrese stock: "));     
+    stock = validarNumber(stock);
 
     let nuevoProducto = new Producto(id, nombre, precio, stock);
 
     tienda.push(nuevoProducto);
 }
+// ----------------------------------
 // funcion para ver todos los productos
 function verProductos() {
     let inventario = "";
@@ -36,21 +61,22 @@ function verProductos() {
     });
     alert(inventario);
 }
+// ----------------------------------
 // funcion para eliminar productos
-
 function borrarProducto() {
     let inventario = "";
     tienda.forEach((producto) => {
         inventario += "-  " + producto.id + "    " + producto.nombre.toUpperCase() + "\n";
-    });    
-    let idc = parseInt(prompt("Elije el ID a elimirar :\n" + inventario));   
-  
-   const productoAEliminar = tienda.findIndex(prod => prod.id === idc);
-   tienda.splice(productoAEliminar, 1);
+    });
+    let idc = parseInt(prompt("Elije el ID a elimirar :\n" + inventario));
+
+    const productoAEliminar = tienda.findIndex(prod => prod.id === idc);
+    tienda.splice(productoAEliminar, 1);
 }
+// ----------------------------------
 // menu con opciones
 function operacion() {
-    let opcion =  parseInt(prompt("Ingrese su operacion :\n1)   Agregar un producto\n2)   Ver producto\n3)   Eliminar producto\n0)   Salir"));
+    let opcion = parseInt(prompt("Ingrese su operacion :\n1)   Agregar un producto\n2)   Ver producto\n3)   Eliminar producto\n0)   Salir"));
     while (opcion !== 0) {
         switch (opcion) {
             case 1:
@@ -66,6 +92,7 @@ function operacion() {
         opcion = parseInt(prompt("Ingrese su operacion :\n1)   Agregar un producto\n2)   Ver producto\n3)   Eliminar producto\n0)   Salir"));
     }
 }
+// ----------------------------------
 // invocacion del menu
 operacion();
 
