50ba78b981e3afacddd70c87fc4dc4797dc6fb0f
kevin.delRio <kevindelrio1993@gmail.com>
2022-08-22 22:34:20 -0300

    incorporacion de busqueda por texto en mercado libre uy

--- javaScript/main.js
+++ javaScript/main.js
@@ -22,6 +22,8 @@ const cantidad = document.getElementById('cantidad')
 const contadorCarrito = document.getElementById('cart-total')
 const checkboxMenor = document.getElementById('menor')
 const checkboxMayor = document.getElementById('mayor')
+const filtros = document.querySelector('.filtro')
+const categorias = document.querySelector('.category')
 let carrito = []
 let productosFiltrados = stockProductos;
 //GET STORAGE
@@ -31,7 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
         actualizarCarrito()
     }
 })
-
 //VACIAR CARRITO
 botonVaciar.addEventListener('click', () => {
     carrito.length = 0
@@ -59,7 +60,6 @@ const agregarAlCarrito = (prodId) => {
     actualizarCarrito()
 }
 //ELIMINAR ELEMENTO DEL CARRITO
-
 const eliminarDelCarrito = (prodId) => {
     const item = carrito.find((prod) => prod.id === prodId)
     const indice = carrito.indexOf(item)
@@ -73,7 +73,6 @@ const actualizarCarrito = () => {
     carrito.forEach((producto) => {
         const div = document.createElement("div")
         div.classList.add("carrito__item")
-
         div.innerHTML = `
 		<img src=${producto.img} alt=${producto.nombre}>
 		<div>
@@ -131,8 +130,8 @@ let category = "";
 function categoryValue() {
     category = document.getElementById("category").value
     productosFiltrados = stockProductos.filter((producto) => producto.categoria === category);
-    checkboxMenor.checked = false; 
-    checkboxMayor.checked = false; 
+    checkboxMenor.checked = false;
+    checkboxMayor.checked = false;
 
     if (productosFiltrados.length > 0) {
         mostrasProductos(productosFiltrados)
@@ -147,13 +146,37 @@ function categoryValue() {
             confirmButtonText: 'Seguir Comprando'
         })
         document.getElementById("category").value = "Todos"
-
     }
-
-
-
 }
-
+// -----------Filtros-Checkbox---------------------
+function menor() {
+    checkboxMayor.checked = false
+    productosFiltrados.sort(function (a, b) {
+        if (a.precio > b.precio) {
+            return 1;
+        }
+        if (a.precio < b.precio) {
+            return -1;
+        }
+        return 0;
+    });
+    console.log(productosFiltrados)
+    mostrasProductos(productosFiltrados)
+}
+function mayor() {
+    checkboxMenor.checked = false
+    productosFiltrados.sort(function (a, b) {
+        if (a.precio < b.precio) {
+            return 1;
+        }
+        if (a.precio > b.precio) {
+            return -1;
+        }
+        return 0;
+    });
+    console.log(productosFiltrados)
+    mostrasProductos(productosFiltrados)
+}
 // Mostrar Productos
 function mostrasProductos(productosCat) {
     document.getElementById("producto").innerHTML = "";
@@ -189,6 +212,7 @@ function mostrasProductos(productosCat) {
         })
     })
 }
+// -----------Retraso---------------------
 function sleep(milliseconds) {
     const date = Date.now();
     let currentDate = null;
@@ -196,37 +220,47 @@ function sleep(milliseconds) {
         currentDate = Date.now();
     } while (currentDate - date < milliseconds);
 }
+// -------------FETCH---------------------
+const API_URL = 'https://api.mercadolibre.com/';
+const endpointProductos = 'sites/MLU/search/';
+let productoss;
+const buscarProductosEnMELI = (productoAbuscar) => {
+    console.log(productoAbuscar)
+    fetch(API_URL + endpointProductos + '?q=' + productoAbuscar)
+        .then((response) => response.json())
+        .then((data) => {
+            productoss = data.results;
 
-// ----------------------------------------------------------------------------------------------------
-function menor() {
-    checkboxMayor.checked = false
-    productosFiltrados.sort(function (a, b) {
-        if (a.precio > b.precio) {
-            return 1;
-        }
-        if (a.precio < b.precio) {
-            return -1;
-        }
-        return 0;
-    });
-    console.log(productosFiltrados)
-    mostrasProductos(productosFiltrados)
+            document.getElementById("producto").innerHTML = "";
+            productoss.forEach((prod) => {
+
+                const div = document.createElement('div')
+                div.classList.add('card')
+                div.innerHTML = ` 
+        <div class="card-img">
+            <div class="max">
+                <img src="${prod.thumbnail}"alt="imagen producto">
+            </div>
+        </div>
+        <div class="card-info">
+        <p class="text-title">${productoAbuscar}</p>
+        <p class="text-body">${prod.title}</p>
+        </div>
+        <div class="card-footer">
+        <span class="text-title">$${prod.price}</span>        
+        <svg class="svg-icon" viewBox="0 0 20 20">
+            <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
+            <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
+            <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
+        </svg>
+        </div>
+    </div>`
+
+                contenedorProductos.appendChild(div)
+                const boton = document.getElementById(`agregar${prod.id}`)
+            })
+
+        })
+    filtros.classList.add("remove-f")
+    categorias.classList.add("remove-f")
 }
-function mayor() {
-    checkboxMenor.checked = false
-    productosFiltrados.sort(function (a, b) {
-        if (a.precio < b.precio) {
-            return 1;
-        }
-        if (a.precio > b.precio) {
-            return -1;
-        }
-        return 0;
-    });
-    console.log(productosFiltrados)
-    mostrasProductos(productosFiltrados)
-}
-
-
-
-// ----------------------------------
